d03d86ca8f64a4497223f9cd3d745b6c27bb34e8
Sebastián Cáceres <ascacere92@gmail.com>
2017-09-18 09:08:56 -0500

    Owa (#4)
    * Added form
    
    * Edited encounter type
    
    * fetching location
    
    * fetching form
    
    * Added button to refresh vitals
    
    * fetch session
    
    * Fetching encountertype
    
    * Fetching the provider, edited the datetime used...
    
    * Creados signos vitales
    
    * Showing correct submit
    
    * changed Close Button's Style

--- owa/app/js/actions/Visits.js
+++ owa/app/js/actions/Visits.js
@@ -36,6 +36,22 @@ export function updateVisitEncounters(visit) {
   }
 }
 
+export function updateVisitEncounters(visit) {
+  let encounters = []
+  return (dispatch) => {
+    try {
+      apiCall(null, "get", `/visit/${visit.uuid}?v=full`)
+      .then((result) => {
+        dispatch(changeSelectedVisit(result))
+        encounters = result.encounters
+        dispatch(fetchObs(encounters))
+      })
+    } catch (e) {
+      console.error("Something happened updating visit...", e)
+    }
+  }
+}
+
 export function receivedActiveVisits(visits) {
   return {
     type: RECEIVED_ACTIVE_VISITS,
